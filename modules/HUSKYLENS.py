990560472bdcdf4e6680f65098e57e70f538786d
Max <sonthaya.nt@hotmail.com>
2022-03-28 20:41:59 +0700

    Update HUSKYLENS.py

--- modules/HUSKYLENS.py
+++ modules/HUSKYLENS.py
@@ -4,8 +4,10 @@ import os
 ADDRESS = 0x32
 
 machine = os.uname().machine
-if "KidBright32" in machine:
+if ("KidBright32" in machine) or ("KidMotor V4" in machine):
     i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+elif "Mbits" in machine:
+    i2c1 = I2C(0, scl=Pin(21), sda=Pin(22), freq=100000)
 else:
     i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
 
